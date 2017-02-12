31cc3a0610e4b89f59ea897061c5522233838479
Noah Petrides <nopetrides@gmail.com>
2017-02-12 13:52:09 +0000

    Player upload 2/12/2017

--- Player.cpp
+++ Player.cpp
@@ -13,22 +13,16 @@ void Player::draw()
 void Player::update()
 {
 	// Akward to try and get + set current Y pos, consider changing
-	int current_pos = m_position.getY();
-	current_pos += gravity;
-	m_position.setY(current_pos);
 
-	if (m_position.getY() > 200)
-	{
-		m_position.setY(200);
-		jumping = false;
-	}
 
-	m_currentFrame = int(((SDL_GetTicks() / 50) % 7));
-	m_acceleration.setX(-0.01); //page 77
+	m_currentFrame = int(((SDL_GetTicks() / 50) % 7)); // Running animation
+	
 
 	handleInput();
+	
 
 	SDLGameObject::update();
+	
 }
 
 void Player::clean()
@@ -37,36 +31,81 @@ void Player::clean()
 
 void Player::handleInput()
 {
-
-	/*Vector2D* target = TheInputHandler::Instance()->getMousePosition(); //page 117
-	m_velocity = *target - m_position;
-	m_velocity /= 50;*/
-
-//	std::cout << "HandleInput called " << "\n";
-//	if(TheInputHandler::Instance()->getMouseButtonState(LEFT))
-//	{
-//		m_velocity.setX(1);
-//	}
-
-//	Vector2D* vec = TheInputHandler::Instance()->getMousePosition();
-//	m_velocity = (*vec - m_position) / 100;
 	if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
 	{
-		m_velocity.setX(2);
+		if (m_velocity.getX() < 0)
+		{
+			
+			m_velocity.setX(m_velocity.getX() + 1);
+		}
+		else if (m_velocity.getX() < 5)
+		{
+			if (!jumping)
+				m_velocity.setX(m_velocity.getX() + 0.5);
+			else
+				m_velocity.setX(m_velocity.getX() + 0.10);
+		}
 	//	std::cout << "Key down right key detected " << "\n";
 	}
-	if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
+	else if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
 	{
-		m_velocity.setX(-2);
+		if (m_velocity.getX() > 0)
+		{
+			m_velocity.setX(m_velocity.getX()  -1);
+		}
+		else if (m_velocity.getX() > -5)
+		{
+			if (!jumping)
+				m_velocity.setX(m_velocity.getX() - 0.5);
+			else
+				m_velocity.setX(m_velocity.getX() - 0.1);
+		}
 	//	std::cout << "Key down left key detected " << "\n";
+	}
+	else
+	{
+		if (m_velocity.getX() > 0.2 || m_velocity.getX() < -0.2)
+		{
+			if (m_velocity.getX() > 0.2)
+				m_velocity.setX(m_velocity.getX() - 0.2);
+			if (m_velocity.getX() < -0.2)
+				m_velocity.setX(m_velocity.getX() + 0.2);
+		}
+		else
+			m_velocity.setX(0);
 	}
 	if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_UP))
 	{
 		//m_velocity.setY(-2);
 		if (!jumping)
 		{
-			jump();
+			m_velocity.setY(-10);
 			jumping = true;
+		}
+		else if (m_velocity.getY() < 0)
+		{
+			m_velocity.setY(m_velocity.getY() + 0.2);
+		}
+		else if (m_position.getY() > 200) // floor collision goes here
+		{
+			m_velocity.setY(0);
+			jumping = false;
+		}
+		else
+		{
+			m_velocity.setY(m_velocity.getY() + 0.5);
+		}
+	}
+	else
+	{
+		if (m_position.getY() > 200) // floor collision goes here
+		{
+			m_velocity.setY(0);
+			jumping = false;
+		}
+		else 
+		{
+			m_velocity.setY(m_velocity.getY() + 0.5);
 		}
 	}
 	if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_DOWN))
@@ -76,13 +115,6 @@ void Player::handleInput()
 	
 }
 
-
-void Player::jump()
-{
-	int current_pos = m_position.getY();
-	current_pos -= 100;
-	m_position.setY(current_pos);
-}
 
 /*
 void Player::draw(SDL_Renderer* pRenderer)
@@ -99,24 +131,4 @@ m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
 }
 */
 
-
-void Player::collideBottom(SDLGameObject* p)
-{
-	m_position.setY(p->getPos().getY() - m_height);
-}
-
-void Player::collideTop()
-{
-	//TODO:: Set Up velocity to 0
-}
-
-void Player::collideRight(SDLGameObject * p)
-{
-	m_position.setX(p->getPos().getX() - m_width);
-}
-
-void Player::collideLeft(SDLGameObject * p)
-{
-	m_position.setX(p->getPos().getX + p->getWidth());
-}
 
